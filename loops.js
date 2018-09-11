8c463aedea24789aaadb55ce535c939f78e615b9
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-11 16:49:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,4 +6,9 @@ function forLoop() {
       console.log (`I am ${i} strange loops.`);
     }
 } 
+
+function forLoop() { 
+  for (var i = 0; i < 25; i++) {
+   console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
 }
